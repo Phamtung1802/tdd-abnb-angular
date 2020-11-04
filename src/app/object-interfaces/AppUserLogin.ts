7bbcae5eced47914b49178e198e3523189e74dd1
Phamtung1802 <phamtung.0218@gmail.com>
2020-11-04 16:22:01 +0700

    Create AppUserLogin.ts

---
+++ src/app/object-interfaces/AppUserLogin.ts
@@ -0,0 +1,10 @@
+import { AppBooking } from './AppBooking';
+import { AppInvoice } from './AppInvoice';
+import { AppProperty } from './AppProperty';
+import { AppReview } from './AppReview';
+import { AppRole } from './AppRole';
+
+export interface AppUserLogin{
+  username: string;
+  password: string;
+}
