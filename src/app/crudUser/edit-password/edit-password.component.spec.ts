210444faa97742e3c6018fde686c6553311920bf
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-11-15 20:00:39 +0700

    sua cac truong ưu tien va password

--- src/app/crudUser/edit-password/edit-password.component.spec.ts
+++ src/app/crudUser/edit-password/edit-password.component.spec.ts
@@ -1,25 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppUser } from 'src/app/object-interfaces/AppUser';
+import { Iloginrequest } from 'src/app/object-interfaces/Iloginrequest';
+import { AppUserService } from 'src/app/service/app-user.service';
 
-import { EditPasswordComponent } from './edit-password.component';
+@Component({
+  selector: 'app-edit-password',
+  templateUrl: './edit-password.component.html',
+  styleUrls: ['./edit-password.component.css']
+})
+export class EditPasswordComponent implements OnInit {
+  currentUser: AppUser = {};
+  userForm: FormGroup;
+  loginRequest: Iloginrequest;
+  success: boolean=false;
+  failure: boolean=false;
+  that = this;
 
-describe('EditPasswordComponent', () => {
-  let component: EditPasswordComponent;
-  let fixture: ComponentFixture<EditPasswordComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditPasswordComponent ]
-    })
-    .compileComponents();
-  });
+  constructor(private appUserService: AppUserService,
+              private fb: FormBuilder,
+              private router: Router) { }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  ngOnInit(): void {
+    this.appUserService.getData().subscribe(data=>{
+      this.currentUser= data;
+    });
+    this.userForm = this.fb.group({
+      oldPassword: new FormControl(),
+      newPassword: new FormControl(),
+      confirmNewPassword: new FormControl()
+    });
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  update(): void {
+    const user = {
+      password: this.userForm.value.newPassword,
+      name: this.userForm.value.oldPassword
+    };
+    console.log(this.userForm.value.newPassword);
+    console.log(this.userForm.value.confirmNewPassword);
+
+    if(this.userForm.value.newPassword===this.userForm.value.confirmNewPassword&&this.userForm.value.newPassword!=null&&this.userForm.value.confirmNewPassword!=null){
+      this.appUserService.updateUserPassword(this.currentUser.id, user).subscribe(data => {
+          this.success=true;
+          this.appUserService.changeData(data);
+        },
+        err=>{
+          this.failure=true;
+          this.success=false;
+        })
+    }
+  }
+}
